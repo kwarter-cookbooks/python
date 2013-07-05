b4b4a9e8e72057767e65d2544e5e842aba497027
Gilles Devaux <gilles.devaux@gmail.com>
2013-07-05 12:19:05 -0700

    new distribute install + template

--- attributes/default.rb
+++ attributes/default.rb
@@ -22,25 +22,25 @@ default['python']['install_method'] = 'package'
 
 if python['install_method'] == 'package'
   case platform
-  when "smartos"
-    default['python']['prefix_dir']         = '/opt/local'
-  else
-    default['python']['prefix_dir']         = '/usr'
+    when "smartos"
+      default['python']['prefix_dir'] = '/opt/local'
+    else
+      default['python']['prefix_dir'] = '/usr'
   end
 else
-  default['python']['prefix_dir']         = '/usr/local'
+  default['python']['prefix_dir'] = '/usr/local'
 end
 
 default['python']['binary'] = "#{python['prefix_dir']}/bin/python"
 
-default['python']['url'] = 'http://www.python.org/ftp/python'
-default['python']['version'] = '2.7.1'
-default['python']['checksum'] = '80e387bcf57eae8ce26726753584fd63e060ec11682d1145af921e85fd612292'
+default['python']['url']               = 'http://www.python.org/ftp/python'
+default['python']['version']           = '2.7.1'
+default['python']['checksum']          = '80e387bcf57eae8ce26726753584fd63e060ec11682d1145af921e85fd612292'
 default['python']['configure_options'] = %W{--prefix=#{python['prefix_dir']}}
 
-default['python']['distribute_script_url'] = 'http://python-distribute.org/distribute_setup.py'
+default['python']['distribute_script_url']              = 'http://python-distribute.org/distribute_setup.py'
 default['python']['distribute_option']['download_base'] = 'https://pypi.python.org/packages/source/d/distribute/'
 
-default['python'][:pip][:config] = false
-default['python'][:pip]['users_group'] = 'sysadmin'
-default['python'][:pip]['cache_dir'] = '/var/pip/cache'
+default['python']['pip']['config']      = false
+default['python']['pip']['users_group'] = 'sysadmin'
+default['python']['pip']['cache_dir']   = '/var/pip/cache'
